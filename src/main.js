02431cd9de5e8ebe52d09aedc2e53ecbd2d53c64
zypang04 <zypang04@gmail.com>
2024-03-09 01:40:00 +0800

    add extra navigation

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.js";
 import MenuA from "./MenuA.tsx";
 import React from 'react'
 import About from './About.js';
-
+import Contact from './Contact.js';
 function Main(){
     return (
         <BrowserRouter>
@@ -12,6 +12,7 @@ function Main(){
                 <Route exact path="/" element={<App/>}/>
                 <Route exact path="/menuA" element={<MenuA/>}/>
                 <Route exact path="/about" element={<About/>}/>
+                <Route exact path="/contact" element={<Contact/>}/>
             </Routes>
             
         </BrowserRouter>
